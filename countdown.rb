e59fff6c9050c8c03f627a690d93954ff5b1cd06
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-27 22:42:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,8 @@ end
 def countdown_with_sleep(number)
   while number >= 1 
     puts "#{number} SECOND(S)!"
-    
+    sleep (1)
     number -= 1
-    return "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
   end
 end
