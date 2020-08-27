f97efabe1f3292d569bcac5e5961596faf36175e
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-27 22:20:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while countdown > 0 
+  while number > 0 
     puts "#{number} SECOND(S)!"
-    countdown -= 1
+    number -= 1
     return "HAPPY NEW YEAR!"
   end
 end
